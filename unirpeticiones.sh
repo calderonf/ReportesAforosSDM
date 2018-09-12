620ce8ec7ba0932ff07dc65893ff0a6f1736678a
Francisco Calderon <calderonf@gmail.com>
2018-09-12 15:10:22 -0500

    listas con reportes de las peticiones 1 a la 31 BUG en ultimo conteo del dia aveces cuenta videos de menos de 15 minutos al final del dia

---
+++ unirpeticiones.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#Guardando copia de seguridad por si acaso.
+cp reportes/reportes_peticiones.csv reportes/reportes_peticiones.csv.old
+#inicia copia de peticiones
+cat peticion1/results/reporte_general_carpeta.csv > reportes/reportes_peticiones.csv
+cat peticion2/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones.csv
+cat peticion3/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones.csv
+cat peticion4/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones.csv
+
+cat peticion5/particion1/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones.csv
+cat peticion5/particion2/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones.csv
+
+cat peticion6/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones.csv
+cat peticion7/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones.csv
+cat peticion8/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones.csv
+cat peticion9/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones.csv
+cat peticion10/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones.csv
+cat peticion11/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones.csv
+cat peticion12/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones.csv
+cat peticion13/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones.csv
+cat peticion14/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones.csv
+cat peticion15/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones.csv
+cat peticion16/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones.csv
+cat peticion17/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones.csv
+cat peticion18/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones.csv
+cat peticion19/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones.csv
+cat peticion20/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones.csv
+cat peticion21/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones.csv
+cat peticion22/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones.csv
+cat peticion23/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones.csv
+cat peticion24/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones.csv
+cat peticion25/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones.csv
+cat peticion26/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones.csv
+cat peticion27/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones.csv
+cat peticion28/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones.csv
+cat peticion29/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones.csv
+cat peticion30/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones.csv
+cat peticion31/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones.csv
