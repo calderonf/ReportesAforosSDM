98af20c78097a9eb2234485b0011e1fa39973cc9
Ubuntu <administrador@SRV-analitica.y2bmml24v3zulk0s3o35qnj0xb.cx.internal.cloudapp.net>
2019-02-08 03:57:22 +0000

    peticiones hasta 208 faltan reportes

--- unirpeticiones.sh
+++ unirpeticiones.sh
@@ -264,6 +264,85 @@ cd reportes
 echo "aproximando hora de archivos, seleccione archivo reportes_peticiones.csv"
 python AproximarHoraArchivos.py
 
+cd ..
+#Peticiones_dia_sin_carro
+#inicia copia de peticiones
+cat peticion277/results/reporte_general_carpeta.csv > reportes/reportes_peticiones_DSC2019.csv
+cat peticion279/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones_DSC2019.csv
+
+cat peticion280/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones_DSC2019.csv
+cat peticion281/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones_DSC2019.csv
+cat peticion282/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones_DSC2019.csv
+cat peticion283/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones_DSC2019.csv
+cat peticion284/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones_DSC2019.csv
+cat peticion285/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones_DSC2019.csv
+cat peticion286/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones_DSC2019.csv
+cat peticion287/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones_DSC2019.csv
+cat peticion288/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones_DSC2019.csv
+cat peticion289/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones_DSC2019.csv
+
+cat peticion290/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones_DSC2019.csv
+cat peticion291/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones_DSC2019.csv
+cat peticion292/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones_DSC2019.csv
+cat peticion293/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones_DSC2019.csv
+cat peticion294/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones_DSC2019.csv
+cat peticion295/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones_DSC2019.csv
+cat peticion296/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones_DSC2019.csv
+cat peticion297/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones_DSC2019.csv
+cat peticion298/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones_DSC2019.csv
+cat peticion299/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones_DSC2019.csv
+
+cat peticion300/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones_DSC2019.csv
+cat peticion301/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones_DSC2019.csv
+cat peticion302/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones_DSC2019.csv
+cat peticion303/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones_DSC2019.csv
+cat peticion304/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones_DSC2019.csv
+cat peticion305/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones_DSC2019.csv
+
+#fin dia sin carro semana antes inicio dia sin carro.
+cat peticion306/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones_DSC2019.csv
+cat peticion307/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones_DSC2019.csv
+cat peticion308/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones_DSC2019.csv
+cat peticion309/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones_DSC2019.csv
+
+cat peticion310/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones_DSC2019.csv
+cat peticion311/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones_DSC2019.csv
+cat peticion312/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones_DSC2019.csv
+cat peticion313/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones_DSC2019.csv
+cat peticion314/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones_DSC2019.csv
+cat peticion315/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones_DSC2019.csv
+cat peticion316/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones_DSC2019.csv
+cat peticion317/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones_DSC2019.csv
+cat peticion318/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones_DSC2019.csv
+cat peticion319/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones_DSC2019.csv
+
+cat peticion320/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones_DSC2019.csv
+cat peticion321/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones_DSC2019.csv
+cat peticion322/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones_DSC2019.csv
+cat peticion323/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones_DSC2019.csv
+cat peticion324/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones_DSC2019.csv
+cat peticion325/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones_DSC2019.csv
+cat peticion326/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones_DSC2019.csv
+cat peticion327/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones_DSC2019.csv
+cat peticion328/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones_DSC2019.csv
+cat peticion329/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones_DSC2019.csv
+
+cat peticion330/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones_DSC2019.csv
+cat peticion331/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones_DSC2019.csv
+cat peticion332/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones_DSC2019.csv
+cat peticion333/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones_DSC2019.csv
+cat peticion334/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones_DSC2019.csv
+cat peticion335/results/reporte_general_carpeta_sinmarco.csv >> reportes/reportes_peticiones_DSC2019.csv
+
+cd reportes
+echo "aproximando hora de archivos, seleccione archivo reportes_peticiones_DSC2019.csv"
+python AproximarHoraArchivos.py
+
+
+
+
+
+
 
 
 
