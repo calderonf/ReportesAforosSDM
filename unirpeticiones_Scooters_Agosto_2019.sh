31bade0869631f42ce71130f2c03064a1543b76e
Francisco Calderon <calderonf@gmail.com>
2019-08-28 06:52:13 -0500

    cambios nuevo dia

--- unirpeticiones_Scooters_Agosto_2019.sh
+++ unirpeticiones_Scooters_Agosto_2019.sh
@@ -1,4 +1,4 @@
-
+#!/bin/bash
 #Peticiones_dia_sin_carro
 #inicia copia de peticiones
 cat /home/francisco/ReportesAforosSDM/Scooters_CAMARA_40_CR11XCL85/12-08-2019/results/reporte_general_carpeta.csv > reportes/reportes_peticiones_Scooters_2019.csv
